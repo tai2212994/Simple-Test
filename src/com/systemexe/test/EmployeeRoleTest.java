0adddbf40b6715ec04e03e0c35af796638aeca9f
tai2212994 <tai2212994@gmail.com>
2019-11-08 07:16:55 +0700

    Refactor Code

--- src/com/systemexe/test/EmployeeRoleTest.java
+++ src/com/systemexe/test/EmployeeRoleTest.java
@@ -6,44 +6,51 @@ import java.util.Scanner;
 
 public class EmployeeRoleTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {	
 		EmployeeRoleTest employee = new EmployeeRoleTest();
-		ArrayList<Employee> ListEmployee = employee.CreateSampleData();
+		ArrayList<Employee> ListEmployee = employee.createSampleData();
+		
 		System.out.println("Danh sach nhan vien:");
 		for (int i = 0; i < ListEmployee.size(); i++) {
 			System.out.println(ListEmployee.get(i).getName());
 		}
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Nhap ten de hien thi chuc vu: ");
-		 String Name = scanner.nextLine();
-		 scanner.close();
-		List<String> Role= employee.findEmployeeRoleByName(ListEmployee, Name);
-		 System.out.println(Role);
-	}
-	public ArrayList<Employee> CreateSampleData(){
-		Employee Employee1 = new Employee("Name1","Project Manager");
-		Employee Employee2 = new Employee("Name2","Business Analyst");
-		Employee Employee3 = new Employee("Name3","Developer");
-		Employee Employee4 = new Employee("Name4","Developer");
-		Employee Employee5 = new Employee("Name3","Tester");
-		ArrayList<Employee> ListEmployee = new ArrayList<Employee>();
-		ListEmployee.add(Employee1);
-		ListEmployee.add(Employee2);
-		ListEmployee.add(Employee3);
-		ListEmployee.add(Employee4);
-		ListEmployee.add(Employee5);
+		String employeeName = scanner.nextLine();
+		scanner.close();
 		
-		return ListEmployee;
-	    
+		List<String> employeeRole = employee.findEmployeeRoleByName(ListEmployee, employeeName);
+		System.out.println(employeeName + ":" + employeeRole);
 	}
-	public List<String> findEmployeeRoleByName(ArrayList<Employee> list, String Name) {
-		List<String> Result = new ArrayList<String>();
+
+	private ArrayList<Employee> createSampleData() {
+		Employee employee1 = new Employee("Name1", "Project Manager");
+		Employee employee2 = new Employee("Name2", "Business Analyst");
+		Employee employee3 = new Employee("Name3", "Developer");
+		Employee employee4 = new Employee("Name4", "Developer");
+		Employee employee5 = new Employee("Name3", "Tester");
+		
+		ArrayList<Employee> employeeList = new ArrayList<Employee>();
+		employeeList.add(employee1);
+		employeeList.add(employee2);
+		employeeList.add(employee3);
+		employeeList.add(employee4);
+		employeeList.add(employee5);
+
+		return employeeList;
+
+	}
+
+	private List<String> findEmployeeRoleByName(ArrayList<Employee> list, String employeeName) {
+		List<String> employeeRoleList = new ArrayList<String>();
+		
 		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getName().equals(Name)) {
-				Result.add(list.get(i).getRole());
+			if (list.get(i).getName().equals(employeeName)) {
+				employeeRoleList.add(list.get(i).getRole());
 			}
 		}
-		return Result;
+		
+		return employeeRoleList;
 	}
 }
